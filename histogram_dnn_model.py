98fbaea6f9774e229378950362524eef32269fc2
Tin Vu <tinvukhac@gmail.com>
2021-03-16 23:18:26 -0700

    Copy master file from HDFS

--- histogram_dnn_model.py
+++ histogram_dnn_model.py
@@ -9,6 +9,7 @@ from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.models import Sequential
+from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.losses import mean_squared_logarithmic_error, mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
 from sklearn import metrics
@@ -42,15 +43,35 @@ class HistogramDNNModel(ModelInterface):
         y_train, y_test, ds1_histograms_train, ds1_histograms_test, ds2_histograms_train, ds2_histograms_test, ds_bops_histogram_train, ds_bops_histogram_test \
             = train_test_split(y, ds1_histograms, ds2_histograms, ds_bops_histogram, test_size=0.2, random_state=42)
 
-        model = HistogramDNNModel.create_cnn(num_rows, num_columns, 1, regress=True)
+        # model = HistogramDNNModel.create_cnn(num_rows, num_columns, 1, regress=True)
+
+        # create CNN model
+        model = Sequential()
+        model.add(Conv2D(16, kernel_size=3, activation='relu', input_shape=(num_rows, num_columns, 1)))
+        model.add(Conv2D(8, kernel_size=3, activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(4, activation='relu'))
+        model.add(Dense(1, activation='linear'))
+
         EPOCHS = 40
         LR = 1e-2
         opt = Adam(lr=LR, decay=LR / EPOCHS)
-        model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
+
+        early_stopping = EarlyStopping(
+            monitor="val_loss",
+            min_delta=0,
+            patience=10,
+            verbose=1,
+            mode="auto",
+            baseline=None,
+            restore_best_weights=True,
+        )
+
+        model.compile(metrics=['mean_absolute_percentage_error'], loss="mean_absolute_percentage_error", optimizer=opt)
         model.fit(
             ds_bops_histogram_train, y_train,
             validation_data=(ds_bops_histogram_test, y_test),
-            epochs=EPOCHS, batch_size=256)
+            epochs=EPOCHS, batch_size=256, callbacks=[early_stopping])
 
         y_pred = model.predict(ds_bops_histogram_test)
 
